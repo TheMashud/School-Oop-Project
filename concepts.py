65ca8850909541c43c3aec47b0bcdc944851290a
TheMashud <2345arham@gmail.com>
2023-08-02 13:15:38 +0600

    School Classes Added

---
+++ concepts.py
@@ -0,0 +1,22 @@
+"""
+
+-------------------------------
+    HIGH LEVEL AREA     
+School System Development
+
+1. student management
+2. Teacher managment
+3. Cafeteria
+4. Sports Managment
+5. club managment
+6. Event Managmrnt
+7. Library Managemnet
+8. Assessment
+---------------------------------------
+Core Functionalities
+1. There Will be a classroom consisting of multiple students
+2. student can enroll multiple classes
+3. Teachers will take classes, they will take exams, the grade the ex exams
+4. system will generate the overall grade
+
+"""
